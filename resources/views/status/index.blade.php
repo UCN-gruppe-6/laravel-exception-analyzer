8d1ca37d2e215c505bbca8db50903e77b752bb93
solesen1992 <saramundal@live.dk>
2025-11-28 12:40:40 +0100

    Making the file work on http://127.0.0.1:8000/driftsstatus

---
+++ resources/views/status/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="UTF-8">
+    <title>Driftsstatus test</title>
+</head>
+<body>
+<h1>Hej fra driftsstatus-viewet 🎉</h1>
+</body>
+</html>
